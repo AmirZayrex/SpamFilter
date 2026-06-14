f6bf3e0f1b5e21dd8db51fe843b54e4814e32e21
Amir Amirjani <amiramirjani0200@gmail.com>
2026-06-14 16:13:16 +0330

    clean config.py

--- src/config.py
+++ src/config.py
@@ -1,17 +1,3 @@
-# import os
-# import sys
-#
-# if getattr(sys, 'frozen', False):
-#     BASE_DIR = os.path.dirname(sys.executable)
-# else:
-#     BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-#
-# MODEL_PATH = os.path.join(BASE_DIR, 'Artifacts', 'spam_model.pkl')
-# DATA_PATH = os.path.join(BASE_DIR, 'Data', 'SMSSpamCollection')
-# ARTIFACTS_DIR = os.path.join(BASE_DIR, 'Artifacts')
-# os.makedirs(ARTIFACTS_DIR, exist_ok=True)
-
-
 import os
 import sys
 
